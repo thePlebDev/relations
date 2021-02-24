14024ef2d2c42bbb9f45d0379d486a9d6eb3cd00
Tristan Elliott <>
2021-02-24 19:35:53 -0400

    additions to the book model

--- src/main/java/com/example/demo/models/Book.java
+++ src/main/java/com/example/demo/models/Book.java
@@ -1,5 +1,46 @@
 package com.example.demo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull; 
+
+@Entity
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	private String name;
+	
+	@ManyToOne(fetch = FetchType.LAZY,optional = false)
+	@JoinColumn(name="library_id")
+	private Library library;
+	
+	public Book() {}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Library getLibrary() {
+		return library;
+	}
+	public void setLibrary(Library library) {
+		this.library = library;
+	}
 }
