55625a436cb437166548ffdafb75b1d2d0baac2a
Tristan Elliott <>
2021-02-24 19:13:44 -0400

    adding the library model

---
+++ src/main/java/com/example/demo/models/Library.java
@@ -0,0 +1,53 @@
+package com.example.demo.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull; 
+
+@Entity
+public class Library {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	private String name;
+	
+	@OneToMany(mappedBy = "library", cascade = CascadeType.ALL)
+	private Set<Book> books = new HashSet<>();
+	
+	public Library () {}
+	
+	//GETTERS AND SETTERS
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Set<Book> getBooks(){
+		return books;
+	}
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+		
+		for(Book b : books) {
+			b.setLibrary(this);
+		}
+	}
+}
